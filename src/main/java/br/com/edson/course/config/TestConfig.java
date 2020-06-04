239b3452f81eff907d5f115b2bf10aab43503100
Edson Junior <edsonkjr@gmail.com>
2020-06-03 22:00:11 -0300

    Payment, one-to-one associat

--- src/main/java/br/com/edson/course/config/TestConfig.java
+++ src/main/java/br/com/edson/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.edson.course.entities.Category;
 import br.com.edson.course.entities.Order;
 import br.com.edson.course.entities.OrderItem;
+import br.com.edson.course.entities.Payment;
 import br.com.edson.course.entities.Product;
 import br.com.edson.course.entities.User;
 import br.com.edson.course.entities.enums.OrderStatus;
@@ -80,7 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		//Fazendo associação do pedido que já foi pago
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
